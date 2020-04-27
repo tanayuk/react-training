8eccb9fc60c5e1d69ddce3bd240c49338a55114a
tanayuk <yuuki.tnk@gmail.com>
2020-04-27 13:14:16 +0900

    Radium導入まで

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import Person from "./Person/Person";
+import Radium from "radium";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { id: "asdf", name: "Yuki", age: 34 },
+      { id: "zdf", name: "Kana", age: 3 },
+      { id: "adlksjfk", name: "Hisae", age: 35 },
+    ],
+    showPersons: false,
+  };
+
+  style = {
+    backgroundColor: "green",
+    color: "white",
+    font: "inherit",
+    border: "1px solid blue",
+    padding: "8px",
+    cursor: "pointer",
+    ":hover": {
+      backgroundColor: "lightgreen",
+      color: "black",
+    },
+  };
+
+  nameChangeHandler = (
+    event: React.FormEvent<HTMLInputElement>,
+    id: string
+  ) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
+    });
+    const person = { ...this.state.persons[personIndex] };
+
+    person.name = event.currentTarget.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({
+      persons: persons,
+    });
+  };
+
+  deletePersonHandler = (personIndex: number) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
+  render() {
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                name={person.name}
+                age={person.age}
+                click={() => this.deletePersonHandler(index)}
+                key={person.id}
+                changed={(event: React.FormEvent<HTMLInputElement>) =>
+                  this.nameChangeHandler(event, person.id)
+                }
+              />
+            );
+          })}
+        </div>
+      );
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push("red");
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push("bold");
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi I'm a react app</h1>
+        <p className={classes.join(" ")}>This is really working</p>
+        <button style={this.style} onClick={this.togglePersonsHandler}>
+          Switch Name
+        </button>
+        {persons}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Radium(App);
---
+++ src/Person/Person.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Person.css";
+import Radium from "radium";
+
+const person = (props: {
+  name: string;
+  age: number;
+  click?: any;
+  changed?: any;
+  children?: string;
+}) => {
+  return (
+    <div className="Person">
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
+};
+
+export default Radium(person);
